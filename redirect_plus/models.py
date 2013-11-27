a6626d6f86ea8c7bb0cafe619ed3189d575a5a4c
Philippe O. Wagner <wagner@arteria.ch>
2013-11-27 13:22:23 +0100

    Fixed model FK

--- redirect_plus/models.py
+++ redirect_plus/models.py
@@ -5,7 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.contrib.redirects.models import Redirect
 
 class RedirectHitCounter(models.Model):
-    redirect = models.ForeignKey('Redirect')
+    redirect = models.ForeignKey(Redirect)
     timestamp = models.DateTimeField(auto_now=False)
     
     def __unicode__(self):
